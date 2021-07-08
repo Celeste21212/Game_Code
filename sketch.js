c069ae9d321d80f645e76d4604eef73b09698818
Celeste21212 <87046811+Celeste21212@users.noreply.github.com>
2021-07-08 13:33:38 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,31 @@
 
 function setup() {
-  createCanvas(400,400);
+
+  createCanvas(1250,570);
+ 
 }
 
 function draw() 
 {
-  background(220);
-
+  
+  if (keyIsDown(UP_ARROW))
+  {
+    background('red');
+  }
+  
+  if (keyIsDown(DOWN_ARROW))
+  {
+    background('orange');
+  }
+  
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    background('yellow');
+  }
+  if (keyIsDown(LEFT_ARROW))
+  {
+    background('green');
+  }
 }
 
 
